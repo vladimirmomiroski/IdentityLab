99e45c88775d34952b712fa3a81b0a4b2730e54f
vladimirmomiroski <83420111+vladimirmomiroski@users.noreply.github.com>
2025-01-24 08:53:06 +0100

    navbar design completed

--- app/components/client/navbar/navbar.tsx
+++ app/components/client/navbar/navbar.tsx
@@ -2,20 +2,21 @@
 
 import React from 'react'
 import Logo from '@/app/components/shared/logo/logo'
+import Button from '@/app/components/shared/button/button'
 
 type Props = {}
 
 const Navbar = (props: Props) => {
     return (
-        <nav>
+        <nav className="py-12 px-10 container flex justify-between items-center mx-auto">
             <div>
-                <Logo width={400} height={400} />
+                <Logo width={100} height={100} />
             </div>
             <div>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
+                <ul className="flex justify-between items-center text-text font-font_family_primary">
+                    <li className="hover:text-color_green cursor-pointer pr-6">Home</li>
+                    <li className="hover:text-color_green cursor-pointer pr-6">Our Story</li>
+                    <Button/>
                 </ul>
             </div>
         </nav>
---
+++ app/components/shared/button/button.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+
+const Button = (props: Props) => {
+
+    return (
+        <a className="font-bold px-4 py-2.5 text-text text-color_secondary bg-color_green font-font_family_secondary rounded">Contact</a>
+    )
+}
+
+export default Button;
